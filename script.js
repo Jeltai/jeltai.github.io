5bc125fea5bbf5b32c8f81883528e854c043e3a2
Jeltai <jeltejonker@gmail.com>
2022-04-20 21:38:22 +0200

    reset

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("button").addEventListener("click", function() {
+document.querySelector(".bg-modal").style.display = "flex";
+});
+
+document.getElementById("register").addEventListener("click", function() {
+    document.querySelector(".bg-modal").style.display = "none";
+    document.querySelector(".bg-modal2").style.display = "flex";
+    });
+
+document.querySelector(".close").addEventListener("click", function() {
+document.querySelector(".bg-modal").style.display = "none";
+});
+
